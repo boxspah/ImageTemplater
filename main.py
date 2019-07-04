2d1b17e264898ab70f32d78993c4d0a6bc9964e5
Wilson Sy <sy.wilson@protonmail.com>
2019-07-04 15:55:53 -0400

    Create main loop structure

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+import os
 from PIL import Image
 
-background = Image.open("test1.png")
-foreground = Image.open("test2.png")
+templates = {}
+images = []
+platform_list = ['Instagram', 'Facebook', 'Twitter', 'Linkedin']
+for p in platform_list:
+    templates[p] = []
 
-background.paste(foreground, (0, 0), foreground)
-background.show()
+for infile in os.listdir("images"):
+    try:
+        with Image.open('images/' + infile) as im:
+            print('Found image: ', infile, im.format, "%dx%d" % im.size, im.mode)
+            images.append(im)
+    except IOError:
+        pass
+
+if len(images) is 0:
+    raise Exception('No images found.')
+
+for im in images:
+    platform = None
+    print(f'Select a platform for {im.filename}:')
+    while platform is None:
+        for i, p in enumerate(platform_list):
+            print(f'\t[{i}] {p}')
+        try: platform = platform_list[int(input())]
+        except IndexError: pass
+
+    if len(templates[platform]) > 0:
+        for t in templates[platform]:
+            print("Image will be processed.")
+    else:
+        print("No appropriate templates found.")
+
+print("--- All images processed. ---")
