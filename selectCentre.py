ba3b88b7807c2fb1383cdf447ed489025b05e1ef
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2019-07-05 11:18:30 -0400

    Scale down large images

--- selectCentre.py
+++ selectCentre.py
@@ -10,11 +10,12 @@ def gotchu():
 def selectCentre(imFile):
     window = Tk()
 
-    cs = Canvas(window, width=500, height=500)
+    cs = Canvas(window, width=800, height=800)
     cs.pack()
 
+    imFile.thumbnail((800, 800))
     imFile = ImageTk.PhotoImage(imFile)
-    image = cs.create_image(500, 0, anchor=NE, image=imFile)
+    image = cs.create_image(400, 400, anchor=CENTER, image=imFile)
     cs.bind("<Button-1>", canvasCallback)
 
     confirm = Button(window, text="Confirm", command=gotchu)
