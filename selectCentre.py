1f12a53ed3d6a007a961d3e0e47a27f8cb7104d8
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2019-07-05 13:19:15 -0400

    Scale canvas to image

--- selectCentre.py
+++ selectCentre.py
@@ -10,15 +10,18 @@ def gotchu():
 def selectCentre(imFile):
     window = Tk()
 
-    cs = Canvas(window, width=800, height=800)
-    cs.pack()
-
-    imFile.thumbnail((800, 800))
+    imFile.thumbnail((1200, 1000))
+    cSize = imFile.size
     imFile = ImageTk.PhotoImage(imFile)
-    image = cs.create_image(400, 400, anchor=CENTER, image=imFile)
+
+    cs = Canvas(window, width=cSize[0], height=cSize[1])
+    cs.pack()
+    image = cs.create_image(cSize[0]//2, cSize[1]//2, anchor=CENTER, image=imFile)
     cs.bind("<Button-1>", canvasCallback)
 
     confirm = Button(window, text="Confirm", command=gotchu)
     confirm.pack()
 
     window.mainloop()
+
+selectCentre(Image.open('images/house-piano-dog.jpg'))
