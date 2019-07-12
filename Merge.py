2c504faaf1c59e4803086b64666b01e1519728f4
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2019-07-12 15:18:14 -0400

    Save processed image

--- Merge.py
+++ Merge.py
@@ -7,10 +7,9 @@ def merge(template, image, mergeData, savefile):
     # crops same percentage of the image as was off-screen in the GUI
     background = background.crop(box=(background.width*mergeData['crop_left'], background.height*mergeData['crop_top'], background.width*(1-mergeData['crop_right']), background.height*(1-mergeData['crop_bottom'])))
 
-    # crops same percentage of the image as was off-screen in the GUI
-    background = background.crop(box=(background.width*mergeData['crop_x'], background.height*mergeData['crop_y'], background.width, background.height))
-    # background = background.resize((int(background.width*ratio), int(background.height*ratio)), Image.ANTIALIAS)
-    # background = background.resize((foreground.width, foreground.height), Image.ANTIALIAS)
+    # downsize template to fit image
+    foreground.thumbnail(background.size, Image.ANTIALIAS)
+
+    # save combined image to file
+    background.paste(foreground, mask=foreground)
     background.save(savefile)
-    # NOTE: alpha_composite only works if foreground and background are the same size
-    # Image.alpha_composite(background, foreground).save(savefile)
