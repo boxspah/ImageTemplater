2e7e37183c598a39bf72e7c48c1cd69e3c4c3b81
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2019-07-15 11:34:02 -0400

    Move platform selection into GUI
    User is given checkbox menu to select destination platforms rather than providing input to the terminal.
    Now, more than 1 platform can be selected.
    
    Closes #5.

---
+++ PlatformSelect.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import warnings
+
+class PlatformSelect:
+    def __init__(self, pList):
+        self.window = tk.Tk()
+        self.window.title('Select destination platform(s)')
+
+        # create dictionary to store checkbox values
+        self.selectedPlatforms = {}
+        for p in pList:
+            self.selectedPlatforms[p] = 0
+
+        # create and pack a checkbox for every platform
+        for p in self.selectedPlatforms:
+            self.selectedPlatforms[p] = tk.IntVar(value=0)
+            l = tk.Checkbutton(self.window, text=p, variable=self.selectedPlatforms[p], onvalue=1, offvalue=0)
+            l.pack()
+
+        # button to proceed
+        self.c = tk.Button(self.window, text='Continue', command=self.confirm)
+        self.c.pack()
+
+        self.window.mainloop()
+
+    def confirm(self):
+        sel_array = [y.get() for x, y in self.selectedPlatforms.items()]
+        # close window and continue only if at least 1 platform is selected
+        if sum(sel_array) < 1:
+            # TODO: Pop-up message saying that no platforms were selected
+            warnings.warn('No platforms selected. Not closing window.')
+        elif sum(sel_array) >= 1:
+            self.window.destroy()
