377fca9e445e9b2870da31fd0085b22eb380892c
Wilson Sy <sy.wilson@protonmail.com>
2019-07-18 14:06:26 -0400

    Change how images are combined
    Paste template and image onto a blank image of correct size.

--- Merge.py
+++ Merge.py
@@ -4,8 +4,6 @@ def merge(template, image, mergeData, savefile):
     background = Image.open(image)
     foreground = Image.open(template)
 
-    magic_ratio = mergeData['magic_ratio']
-
     # crops same percentage of the image as was off-screen in the GUI
     background = background.crop(box=(background.width*mergeData['crop_left'], background.height*mergeData['crop_top'], background.width*(1-mergeData['crop_right']), background.height*(1-mergeData['crop_bottom'])))
 
@@ -13,5 +11,7 @@ def merge(template, image, mergeData, savefile):
     foreground = foreground.resize(tuple(int(dim*mergeData['magic_ratio']) for dim in foreground.size), resample=Image.LANCZOS)
 
     # save combined image to disk
-    background.paste(foreground, mask=foreground)
-    background.save(savefile)
+    collage = Image.new('RGBA', foreground.size, color=(255, 255, 255, 0))
+    collage.paste(background)
+    collage.paste(foreground, mask=foreground)
+    collage.save(savefile)
