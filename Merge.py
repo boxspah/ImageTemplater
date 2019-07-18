35fcb1515da573d1da00e3399cf1f67adeed368d
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2019-07-18 15:13:10 -0400

    Adjust for image offsets
    also:
     - store crop percentages in a 4-tuple
    
    Closes #12.

--- Merge.py
+++ Merge.py
@@ -5,13 +5,13 @@ def merge(template, image, mergeData, savefile):
     foreground = Image.open(template)
 
     # crops same percentage of the image as was off-screen in the GUI
-    background = background.crop(box=(background.width*mergeData['crop_left'], background.height*mergeData['crop_top'], background.width*(1-mergeData['crop_right']), background.height*(1-mergeData['crop_bottom'])))
+    background = background.crop(box=(background.width*mergeData['crop'][0], background.height*mergeData['crop'][1], background.width*(1-mergeData['crop'][2]), background.height*(1-mergeData['crop'][3])))
 
     # downsize template to fit image
     foreground = foreground.resize(tuple(int(dim*mergeData['magic_ratio']) for dim in foreground.size), resample=Image.LANCZOS)
 
     # save combined image to disk
     collage = Image.new('RGBA', foreground.size, color=(255, 255, 255, 0))
-    collage.paste(background)
+    collage.paste(background, box=(int(foreground.width*mergeData['offset'][0]), int(foreground.height*mergeData['offset'][1])))
     collage.paste(foreground, mask=foreground)
     collage.save(savefile)
